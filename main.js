844300ad5d7bb0decfecf7b09ef5b65efc333eb4
alex-png <alexrod102797@gmail.com>
2020-03-18 23:36:32 -0400

    Created a conditional that runs when a character that's assigned as a key exists in the character map; it compares the value of the key to the character of the second string in the index being iterated over, and if they don't match, then the strings are not mappable, logging false.

--- main.js
+++ main.js
@@ -7,12 +7,17 @@ function isMappable(s1, s2) {
     const charMap = {}
 
     for (let i = 0; i < s1.length; i++) {
-
-        if (!charMap[s1[i]]) {
+        if (charMap[s1[i]]) {
+            if (charMap[s1[i]] !== s2[i]) {
+                return console.log('false')
+            }
+        } else {
             charMap[s1[i]] = s2[i]
         }
-
     }
 
     return console.log('true')
 }
+
+
+isMappable(s1, s2)
