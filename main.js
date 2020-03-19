b38e7f4c71acc8654133e981b23bc2dfd9fb0c54
alex-png <alexrod102797@gmail.com>
2020-03-18 23:26:05 -0400

    created character map for strings by iterating over first string, if char doesnt exist as a key in charMap, then give it the value of the corresponding char in the second string

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-function isMappable(s1, s2){
-    
-    if(s1.length !== s2.length ){
+function isMappable(s1, s2) {
+
+    if (s1.length !== s2.length) {
         return console.log('false')
     }
-    
+
+    const charMap = {}
+
+    for (let i = 0; i < s1.length; i++) {
+
+        if (!charMap[s1[i]]) {
+            charMap[s1[i]] = s2[i]
+        }
+
+    }
+
     return console.log('true')
 }
-
